6d1574b76f992517355346c4d74ec935c7c03965
ILLIA89 <anolij1@gmail.com>
2023-07-31 21:32:47 +0200

    task 2.1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from '@vimeo/player';
+
+import { throttle } from 'lodash';
+
+// звернення до iframe  в DOM
+const iframe = document.querySelector('iframe');
+
+// ініціалізація бібліотеки 
+const player = new Player(iframe, {
+ loop: true,
+  fullscreen: true,
+  quality: 'auto',
+});
+// запис ключа в сховище 
+const localStorageKey = 'videoplayer-current-time';
+
+// відстеження оновлення часу
+
+player.on('timeupdate', throttle((e) => {
+    localStorage.setItem(localStorageKey, e.seconds); // Час відтворення оновлюється у сховищі не частіше, ніж раз на секунду
+}, 1000));
+
+// Перевірка наявності методу setCurrentTime та відновлення часу відтворення зі збереженої позиції під час перезавантаження сторінки.
+if (player.setCurrentTime) {
+  const currentTime = localStorage.getItem(localStorageKey);
+  if (currentTime) {
+    player.setCurrentTime(currentTime);
+  }
+}
+
+
 
