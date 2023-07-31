fd7574b7e7ea83f93c8869e661be91b0cc92efed
ILLIA89 <anolij1@gmail.com>
2023-07-31 18:44:25 +0200

    task-1.1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,43 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// підключення бібліотеки 
+import SimpleLightbox from 'simplelightbox';
 
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryContainer = document.querySelector('.gallery');
+
+// Використання методу reduce() - створення галереї картинок
+const markup = galleryItems.reduce((acc, { preview, original, description }) => acc + `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+      loading="lazy" 
+    />
+  </a>
+</li>`, "");
+// додавання створених картинок
+galleryContainer.insertAdjacentHTML("beforeend", markup);
+// в цій частині коду застосовуємо бібліотеку SimpleLightbox
+let lightbox = new SimpleLightbox('.gallery a', {
+        captions: 'true',
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250,
+     });
+// в цій частині коду реалізуємо делегування подій і відкриття бібліотеки при натисканні на картинку
+galleryContainer.addEventListener('click', (e) => {
+  e.preventDefault();
+  
+  if (e.target.classList.contains("gallery__image"))
+    
+    lightbox.open()
+     })
+
+
+console.log(galleryItems);
 console.log(galleryItems);
