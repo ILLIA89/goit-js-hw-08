f48ac8c7ca67ac223332b2acd3ca3130d6169475
ILLIA89 <anolij1@gmail.com>
2023-08-01 20:04:46 +0200

    task-3.2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,7 +2,8 @@ import { throttle } from 'lodash';
 
 const form = document.querySelector('.feedback-form');
 const LOCALSTORAGE_KEY = 'feedback-form-state';
-
+const emailInput = document.querySelector('input[name="email"]');
+const messageTextarea = document.querySelector('textarea[name="message"]');
 
 
 let data = {};
@@ -18,19 +19,48 @@ function onInput(e) {
     localStorage.setItem(LOCALSTORAGE_KEY,
         JSON.stringify({ email: data.email, message: data.message }));
 }
+
+// Метод load який абстрагує повторюваний код перевірки помилок
+const load = key => {
+    try {
+        const serializedState = localStorage.getItem(key);
+        // перевіряю чи данні не порожні якщо так повертаю undefined
+        if (!serializedState) return undefined;
+
+        return JSON.parse(serializedState)
+    } catch (error) {
+        // повертається повідомлення підчас помилки
+        console.error('Get state error: ', error.message);
+        return undefined;
+    }
+};
+
+
 function onSubmit (e) {
     e.preventDefault();
-    console.log(data);
-    clearForm();
+    if (emailInput.value === '' || messageTextarea.value === '') {
+        return alert('Заповніть всі поля!');
+    }
+
+    console.log({ email: emailInput.value, message: messageTextarea.value });
+        clearForm();
 }
 
 
 function clearForm () {
     data = {}
     localStorage.removeItem(LOCALSTORAGE_KEY);
-    const emailInput = document.querySelector('input[name="email"]');
-    const messageTextarea = document.querySelector('textarea[name="message"]');
     emailInput.value = '';
   messageTextarea.value = '';
 }
 
+// перевірка сховища 
+
+//  заповнювати поля данними якщо в сховищі є данні
+
+const storageData = load(LOCALSTORAGE_KEY);
+if (storageData) {
+    const { email: storedEmail, message: storedMessage } = storageData;
+    emailInput.value = storedEmail;
+    messageTextarea.value = storedMessage;
+}
